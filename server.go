3bea7133f0ecffe5c735b1de21a5f52754b13be9
yaw boakye-owusu <yaw.owusu@hotmail.com>
2021-08-24 09:12:20 +0100

    updated message

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"Welcome to the HomePage")
-	fmt.Println("Endpoint Hit: homePage")
+	fmt.Println("Endpoint Hit: home page")
 }
 
 func handleRequest()  {
